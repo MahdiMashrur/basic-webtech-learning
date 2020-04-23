5bf73854317aa7d2a2b8691ab6e2b57bcd67797c
MahdiMashrur <mashrur123@gmail.com>
2020-04-24 02:03:13 +0600

    practicing alert, prompt, confirm - lesson 2.9

---
+++ javascript/javascript.info tutorials/2.9 alert prompty confirm/2.9 alert.js
@@ -0,0 +1,5 @@
+alert("vrum");
+let age= prompt("What is your age",50);
+alert("your given age was= "+age);
+let isCovid19Positive= confirm("Are you covid-19 positive") ;
+alert("You are covid-19 positive is "+isCovid19Positive);

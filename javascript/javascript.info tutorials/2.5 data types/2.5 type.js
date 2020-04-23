4e8def15b924f5b3773080a7fc85a30935e8ab88
MahdiMashrur <mashrur123@gmail.com>
2020-04-23 19:22:09 +0600

    learning types 2.5

---
+++ javascript/javascript.info tutorials/2.5 data types/2.5 type.js
@@ -0,0 +1,19 @@
+let name = "Ilya"; // string
+
+console.log( `hello ${1}` ); // ?
+
+console.log( `hello ${"name"}` ); // ?
+
+console.log( `hello ${name}` ); // ?
+
+
+let isReady= true; // boolean
+let count=9; //number
+const bigInt= 12345678901234567890234567890234567890n ;
+
+console.log("name typof "+typeof(name));
+console.log("isReady typof "+typeof(isReady));
+console.log("count typof "+typeof(count));
+console.log("bigInt typof "+typeof(bigInt));
+console.log("null typof "+typeof(null));
+console.log("undefined typof "+typeof(undefined));

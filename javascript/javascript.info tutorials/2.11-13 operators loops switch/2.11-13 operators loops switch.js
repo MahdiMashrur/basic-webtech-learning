5abae938968809b0c5287af94ac26e4de5be7a6f
MahdiMashrur <mashrur123@gmail.com>
2020-04-27 13:38:22 +0600

    learning operators, loops, switch

---
+++ javascript/javascript.info tutorials/2.11-13 operators loops switch/2.11-13 operators loops switch.js
@@ -0,0 +1,49 @@
+//switch practice :
+
+let a = +prompt('a?', '');
+switch (a) {
+  case 0:
+    alert( 0 );
+    break;
+  case 1:
+    alert( 1 );
+    break;
+  case 2:
+  case 3:
+    alert( '2,3' );
+    break;
+   
+}
+
+/*
+We covered 3 types of loops:
+
+while – The condition is checked before each iteration.
+do..while – The condition is checked after each iteration.
+for (;;) – The condition is checked before each iteration, additional settings available.
+To make an “infinite” loop, usually the while(true) construct is used. Such a loop, just like any other, can be stopped with the break directive.
+
+If we don’t want to do anything in the current iteration and would like to forward to the next one, we can use the continue directive.
+
+break/continue support labels before the loop. A label is the only way for break/continue to escape a nested loop to go to an outer one.
+*/
+
+outer: for (let i = 0; i < 3; i++) {
+
+  for (let j = 0; j < 3; j++) {
+
+    let input = prompt(`Value at coords (${i},${j})`, '');
+
+    // if an empty string or canceled, then break out of both loops
+    if (!input) break outer; // (*)
+
+    // do something with the value...
+  }
+}
+alert('Done!');
+
+// breaks to an outer for-loop
+
+
+//**** OR “||” finds the first truthy value
+// ***** AND “&&” finds the first falsy value
